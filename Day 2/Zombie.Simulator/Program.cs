82db56d2c0fead1c3973a8a0a573b995dc982d8c
Elonzo Duncan <legionscare@gmail.com>
2018-05-26 15:38:10 -0400

    woo-ha

--- Day 2/Zombie.Simulator/Program.cs
+++ Day 2/Zombie.Simulator/Program.cs
@@ -7,6 +7,12 @@ namespace Zombie.Simulator
         static void Main(string[] args)
         {
             Person Me = new Person();
+
+            Me.DistanceTraveled = 5;
+            Console.WriteLine(Me.DistanceTraveled + " miles traveled");
+
+            Me.Walk(10);
+            Console.WriteLine("Distance traveled for 2 minutes = " + Me.DistanceTraveled + " miles traveled");
         }
     }
 }
--- Day 2/Zombie.Simulator/Zombie.Classes.cs
+++ Day 2/Zombie.Simulator/Zombie.Classes.cs
@@ -2,8 +2,20 @@ using System;
 
 namespace Zombie.Simulator
 {
-    class Person
+    public class Person
     {
+        public int DistanceTraveled{get; set;}
+
+        public void Walk(decimal minutes)
+        {
+            DistanceTraveled = CalculateWalkDistance(minutes);
+        }
+
+        private int CalculateWalkDistance(decimal minutes)
+        {
+            return ((int) minutes) * 2;
+        }
+
         public Person()
         {
             Console.WriteLine("A new person has been created.");
