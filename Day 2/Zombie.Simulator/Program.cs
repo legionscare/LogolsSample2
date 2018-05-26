2b864a4ebe03f21a85b90399824cd15561943e63
Elonzo Duncan <legionscare@gmail.com>
2018-05-26 13:00:22 -0400

    with new includes

--- Day 2/Zombie.Simulator/Program.cs
+++ Day 2/Zombie.Simulator/Program.cs
@@ -2,15 +2,6 @@
 
 namespace Zombie.Simulator
 {
-
-    class Person
-    {
-            public Person()
-            {
-                Console.WriteLine("A new person has been created.");
-            }
-    }
-
     class Program
     {
         static void Main(string[] args)
---
+++ Day 2/Zombie.Simulator/Zombie.Classes.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Zombie.Simulator
+{
+    class Person
+    {
+        public Person()
+        {
+            Console.WriteLine("A new person has been created.");
+        }
+    }
+}
