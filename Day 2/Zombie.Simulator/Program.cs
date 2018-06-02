e6b4a4b690378bde19f97058915a813d0cdd28d5
Elonzo Duncan <legionscare@gmail.com>
2018-06-02 10:41:11 -0400

    Done

--- Day 2/Zombie.Simulator/Program.cs
+++ Day 2/Zombie.Simulator/Program.cs
@@ -6,13 +6,26 @@ namespace Zombie.Simulator
     {
         static void Main(string[] args)
         {
-            Person Me = new Person();
+            Human Me = new Human();
 
             Me.DistanceTraveled = 5;
             Console.WriteLine(Me.DistanceTraveled + " miles traveled");
 
-            Me.Walk(10);
-            Console.WriteLine("Distance traveled for 2 minutes = " + Me.DistanceTraveled + " miles traveled");
+            Me.Walk(2);
+            Console.WriteLine("Walk distance traveled for 2 minutes = " + Me.DistanceTraveled + " miles traveled");
+
+            Me.Run(2);
+            Console.WriteLine("Run distance traveled for 2 minutes = " + Me.DistanceTraveled + " miles traveled");
+
+            Console.WriteLine("");
+
+            Zombie Deadite = new Zombie();
+
+            Deadite.DistanceTraveled = 15;
+            Console.WriteLine(Deadite.DistanceTraveled + " miles traveled");
+
+            Deadite.Walk(2);
+            Console.WriteLine("Walk distance traveled for 2 minutes = " + Deadite.DistanceTraveled + " miles traveled");
         }
     }
 }
--- Day 2/Zombie.Simulator/Zombie.Classes.cs
+++ Day 2/Zombie.Simulator/Zombie.Classes.cs
@@ -2,23 +2,49 @@ using System;
 
 namespace Zombie.Simulator
 {
-    public class Person
+    public abstract class Person
     {
         public int DistanceTraveled{get; set;}
 
-        public void Walk(decimal minutes)
+        public virtual void Walk(decimal minutes)
         {
             DistanceTraveled = CalculateWalkDistance(minutes);
         }
 
-        private int CalculateWalkDistance(decimal minutes)
+        public virtual int CalculateWalkDistance(decimal minutes)
         {
-            return ((int) minutes) * 2;
+            return ((int) minutes) * 10;
         }
 
         public Person()
         {
             Console.WriteLine("A new person has been created.");
+        }
+    }
+
+    public sealed class Human : Person
+    {
+        public Human()
+        {
+            Console.WriteLine("A new human has arrived.");
+        }
+
+        public void Run(decimal minutes)
+        {
+            DistanceTraveled = CalculateWalkDistance((int) minutes) * 5;
+        }
+    }
+
+    public sealed class Zombie : Person
+    {
+        public Zombie()
+        {
+            Console.WriteLine("A new zombie has arrived.");
+        }
+
+        public override int CalculateWalkDistance(decimal minutes)
+        {
+            return ((int) minutes) / 2;
         }
     }
 }
